cb75b266754859170daf3a62d9b0e953664d1448
Jinho <dfghcvb11@naver.com>
2022-10-07 21:14:48 +0900

    Initial commit

---
+++ src/test/java/com/jinho/mvcblog/MvcBlogApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jinho.mvcblog;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MvcBlogApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
